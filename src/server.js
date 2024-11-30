6d35a5a52af93811001bf3a07c59d1fb17713e5c
toanla05 <toanthienla2005@gmail.com>
2024-11-30 14:57:17 +0700

    Initial Express Boilerplate

--- src/server.js
+++ src/server.js
@@ -1,10 +1,26 @@
 import express from 'express';
+import { mapOrder } from '~/utils/sorts.js';
+
 const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World');
+const hostname = 'localhost';
+const port = 8017;
+
+app.get('/', (req, res) => {
+  // Test Absolute import mapOrder
+  console.log(mapOrder(
+    [{ id: 'id-1', name: 'One' },
+    { id: 'id-2', name: 'Two' },
+    { id: 'id-3', name: 'Three' },
+    { id: 'id-4', name: 'Four' },
+    { id: 'id-5', name: 'Five' }],
+    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
+    'id'
+  ));
+  res.end('<h1>Hello World!</h1><hr>');
 });
 
-app.listen(3000, () => {
-  console.log('Server running...');
+app.listen(port, hostname, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Hello Trung Quan Dev, I am running at ${hostname}:${port}/`);
 });
