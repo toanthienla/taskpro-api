a0a077e6dd4986da5c79aeb9d63478852d2f0b7c
toanla05 <toanthienla2005@gmail.com>
2024-12-02 23:31:03 +0700

    Add connect mongodb and setup env

---
+++ src/config/mongodb.js
@@ -0,0 +1,36 @@
+import { MongoClient, ServerApiVersion } from 'mongodb';
+import { env } from '~/config/enviroment.js';
+
+
+const MONGODB_URI = env.MONGODB_URI;
+const DATABASE_NAME = env.DATABASE_NAME;
+
+let taskproDb = null;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true
+  },
+  autoSelectFamily: false
+}
+);
+
+export const CONNECT_DB = async () => {
+  await client.connect();
+  taskproDb = client.db(DATABASE_NAME);
+};
+
+export const CLOSE_DB = async () => {
+  await client.close();
+};
+
+export const GET_DB = () => {
+  if (taskproDb) {
+    return taskproDb;
+  }
+  else
+    throw new Error('Something go wrong in get Monodb!');
+};
--- src/server.js
+++ src/server.js
@@ -1,26 +1,37 @@
+/* eslint-disable no-console */
 import express from 'express';
-import { mapOrder } from '~/utils/sorts.js';
+import { CONNECT_DB, GET_DB, CLOSE_DB } from '~/config/mongodb';
+import exitHook from 'async-exit-hook';
+import { env } from '~/config/enviroment.js';
 
 const app = express();
 
-const hostname = 'localhost';
-const port = 8017;
+const hostname = env.APP_HOST;
+const port = env.APP_PORT;
 
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  console.log(mapOrder(
-    [{ id: 'id-1', name: 'One' },
-    { id: 'id-2', name: 'Two' },
-    { id: 'id-3', name: 'Three' },
-    { id: 'id-4', name: 'Four' },
-    { id: 'id-5', name: 'Five' }],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ));
-  res.end('<h1>Hello World!</h1><hr>');
-});
+const START_SERVER = () => {
+  app.get('/', async (req, res) => {
+    console.log(await GET_DB().listCollections().toArray());
+    res.end('<h1>Hello Wolrd</h1>');
+  });
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello Trung Quan Dev, I am running at ${hostname}:${port}/`);
-});
+  app.listen(port, hostname, () => {
+    console.log(`Hello ToanLa, I am running at http://${hostname}:${port}/`);
+  });
+
+  exitHook(() => {
+    console.log('Clean Mongodb');
+    CLOSE_DB();
+  });
+};
+
+// An IIFE (Immediately Invoked Function Expression)
+(async () => {
+  try {
+    await CONNECT_DB();
+    START_SERVER();
+  } catch (error) {
+    console.error(error);
+    process.exit(0);
+  }
+})();
