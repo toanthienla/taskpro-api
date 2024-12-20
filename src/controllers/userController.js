388fa77bd0e7fa9a869f1050978ab4bbe4ff3b51
toanla05 <toanthienla2005@gmail.com>
2024-12-20 18:42:00 +0700

    Update JWT  cookies verify user and Redux persist

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,6 @@
 import { StatusCodes } from 'http-status-codes';
 import { userService } from '~/services/userService';
+import ms from 'ms';
 
 const createUser = async (req, res, next) => {
   try {
@@ -23,7 +24,19 @@ const loginUser = async (req, res, next) => {
   try {
     const result = await userService.loginUser(req.body);
 
-    console.log(result);
+    // Return handle http only cookie
+    res.cookie('accessToken', result.accessToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'none',
+      maxAge: ms('14 days')
+    });
+    res.cookie('refreshToken', result.refreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'none',
+      maxAge: ms('14 days')
+    });
 
     res.status(StatusCodes.OK).json(result);
   } catch (error) {
